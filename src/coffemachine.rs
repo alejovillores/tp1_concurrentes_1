6983c7ba92ecd5e634a93e10a9ef57ed6acd57c1
alejovillores <avillores@fi.uba.ar>
2023-04-07 19:52:41 -0300

    feat: add conditional varible to signal new ticket

--- src/coffemachine.rs
+++ src/coffemachine.rs
@@ -24,23 +24,32 @@ impl CoffeMachine {
                 return Ok(true);
             }
         };
-        Err("[error] - monitor failed".to_string())
+        Err("[error] - machine ready monitor failed".to_string())
+    }
+
+    pub fn notify_new_ticket(&self, lock: &Mutex<Ticket>, cvar: &Condvar) -> Result<(), String> {
+        if let Ok(mut ticket) = lock.lock() {
+            ticket.ready();
+            cvar.notify_all();
+            return Ok(());
+        };
+        Err("[error] - ticket monitor failed".to_string())
     }
 
     #[allow(dead_code)]
     pub fn start(&self) {
         let monitor = Arc::new((Mutex::new(false), Condvar::new()));
-
         loop {
             let (lock, cvar) = &*monitor;
             match self.finished(lock, cvar) {
                 Ok(_) => {
-                    // read new line
+                    // TODO: read new line
                     // create ticket
                     let _ticket = Ticket::new(10);
                 }
                 Err(e) => {
                     println!("{:?}", e);
+                    break;
                 }
             }
         }
@@ -63,7 +72,7 @@ impl Default for CoffeMachine {
 mod coffemachine_test {
     use std::sync::{Arc, Condvar, Mutex};
 
-    use crate::coffemachine::CoffeMachine;
+    use crate::{coffemachine::CoffeMachine, ticket::Ticket};
 
     #[test]
     fn it_should_initialize_with_0_coffe_made() {
@@ -81,5 +90,29 @@ mod coffemachine_test {
             Ok(result) => assert!(result),
             Err(_) => assert!(false),
         }
+    }
+
+    #[test]
+    fn it_should_signal_coffe_dispenser() {
+        let coffemachine: CoffeMachine = CoffeMachine::new();
+        let ticket = Ticket::new(10);
+        let monitor = Arc::new((Mutex::new(ticket), Condvar::new()));
+
+        let monitor_clone = monitor.clone();
+        let (lock, cvar) = &*monitor;
+        let (lock_clone, cvar_clone) = &*monitor_clone;
+
+        let _ = coffemachine
+            .notify_new_ticket(lock_clone, cvar_clone)
+            .unwrap();
+
+        if let Ok(guard) = lock.lock() {
+            match cvar.wait_while(guard, |ticket: &mut Ticket| ticket.is_not_ready()) {
+                Ok(result) => assert!(!result.is_not_ready()),
+                Err(_) => assert!(false),
+            };
+        } else {
+            assert!(false)
+        };
     }
 }
--- src/ticket.rs
+++ src/ticket.rs
@@ -1,10 +1,23 @@
 #[derive(Debug)]
 pub struct Ticket {
     _coffe_amount: i32,
+    not_ready: bool,
 }
 
 impl Ticket {
     pub fn new(_coffe_amount: i32) -> Self {
-        Self { _coffe_amount }
+        let not_ready = true;
+        Self {
+            _coffe_amount,
+            not_ready,
+        }
+    }
+
+    pub fn ready(&mut self) {
+        self.not_ready = false
+    }
+
+    pub fn is_not_ready(&self) -> bool {
+        self.not_ready
     }
 }
