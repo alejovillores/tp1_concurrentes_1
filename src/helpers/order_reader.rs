d1e4ee7fa746e90f39e3ae76a18afbfe8ac17451
alejovillores <avillores@fi.uba.ar>
2023-04-22 13:15:25 -0300

    feat: add map orderJSON to oder

--- src/helpers/order_reader.rs
+++ src/helpers/order_reader.rs
@@ -1,3 +1,4 @@
+use std::collections::VecDeque;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
@@ -20,15 +21,21 @@ impl OrderJSON {
             cacao_amount,
         }
     }
+
+    pub fn to_order(&self) -> Order {
+        Order::new(self.coffee_amount, self.water_amount, self.cacao_amount)
+    }
 }
 
 pub struct OrderReader {
     path: String,
+    orders: VecDeque<OrderJSON>
 }
 
 impl OrderReader {
     pub fn new(path: String) -> Self {
-        Self { path }
+        let orders: VecDeque<OrderJSON> = VecDeque::new();
+        Self { path, orders }
     }
 
     #[allow(unused_must_use)]
@@ -47,12 +54,39 @@ impl OrderReader {
         }
     }
 
-    fn make_orders(&self, orders: String) -> Result<Vec<OrderJSON>, String> {
-        match serde_json::from_str::<Vec<OrderJSON>>(&orders) {
+    fn make_orders(&self, orders: String) -> Result<VecDeque<OrderJSON>, String> {
+        match serde_json::from_str::<VecDeque<OrderJSON>>(&orders) {
             Ok(r) => Ok(r),
             Err(_) => Err("could not convert json to object".to_string()),
         }
     }
+    
+    #[allow(unused_must_use)]
+    pub fn read_json(&mut self) -> Result<(),String>{
+        match self.read_file() {
+            Ok(s) => {
+                if let Ok(o) = self.make_orders(s){
+                    self.orders = o;
+                    Ok(())
+                }else {
+                    Err("could not make orders".to_string())
+                }
+            }
+            Err(e)=> Err(e.to_string()),
+        }
+    }
+
+
+    pub fn get_order(&mut self) -> Option<Order> {
+        match self.orders.pop_front(){
+            Some(o) => {
+                let order = o.to_order();
+                Some(order)
+            },
+            None => None,
+        }
+    }
+
 }
 
 #[cfg(test)]
@@ -89,4 +123,34 @@ mod order_reader_test {
 
         assert_eq!(result.get(0).unwrap().water_amount, 20)
     }
+
+    
+    #[test]
+    fn it_should_read_4_orders() {
+        let o_reader = OrderReader::new("res/orders.test2.json".to_owned());
+
+        let res = o_reader.read_file().unwrap();
+        let result = o_reader.make_orders(res).unwrap();
+
+        assert_eq!(result.len(), 4)
+    }
+
+    #[test]
+    fn it_should_init_orders() {
+        let mut o_reader = OrderReader::new("res/orders.test2.json".to_owned());
+        o_reader.read_json().unwrap();
+        
+        assert_eq!(o_reader.orders.len(), 4)
+    }
+
+    #[test]
+    fn it_should_map_order_json_to_order() {
+        let mut o_reader = OrderReader::new("res/orders.test2.json".to_owned());
+        o_reader.read_json().unwrap();
+        let order = o_reader.get_order().unwrap();
+        
+        assert!(order.is_not_ready())
+    }
+
+
 }
